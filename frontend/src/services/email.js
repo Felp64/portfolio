90b6bd349636ee12784bd287c618e0d6eed9b916
Felipe Augusto Mendes Pereira <felipepepe64@gmail.com>
2026-03-11 21:04:46 -0300

    Up email JS

--- frontend/src/services/email.js
+++ frontend/src/services/email.js
@@ -2,9 +2,9 @@ import emailjs from '@emailjs/browser';
 /*serviceID,templateID,publicKey devem ser pegos no site do emailjs. As variáveis do template
 devem ser name, email e message, para coincidir com as variáveis do formulário do EmailForm.jsx
 */
-const serviceID = 'YOUR_SERVICE_ID';
-const templateID = 'YOUR_TEMPLATE_ID';
-const publicKey = 'YOUR_PUBLIC_KEY';
+const serviceID = 'service_glogsfm';
+const templateID = 'template_9s1pm4c';
+const publicKey = 'KfzlHO7xzvPXSYiMO';
 
 export function sendEmail(formRef) {
   if (!formRef?.current) {
